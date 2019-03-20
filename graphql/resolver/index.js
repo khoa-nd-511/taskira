e7754bdb4a704fc4fb2a25cb0d1e39116ce29f1a
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-03-20 23:49:07 +0700

    Added more end points and restrucured files

---
+++ graphql/resolver/index.js
@@ -0,0 +1,156 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const Fawn = require('fawn');
+
+const Ticket = require('../../models/ticket');
+const User = require('../../models/user');
+
+Fawn.init(mongoose);
+
+const mapTicketData = (ticketData, hasAssignee = false) => {
+  let mappedTicketData = {
+    ...ticketData._doc,
+    creator: queryUser.bind(this, ticketData.creator)
+  };
+
+  if (hasAssignee) {
+    mappedTicketData['assignee'] = queryUser.bind(this, ticketData.assignee)
+  }
+
+  return mappedTicketData;
+};
+
+const mapUserData = (userData, hasAssignedTicket = false) => {
+  if (hasAssignedTicket) {
+    return {
+      ...userData._doc,
+      createdTickets: queryTickets.bind(this, userData.createdTickets),
+      assignedTickets: queryTickets.bind(this, assignedTickets)
+    }
+  }
+
+  return {
+    ...userData._doc,
+    createdTickets: queryTickets.bind(this, userData.createdTickets)
+  }
+}
+
+const queryUser = async userId => {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found!")
+
+    return mapUserData(user);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const queryTickets = async ticketIds => {
+  try {
+    const tickets = await Ticket.find({ _id: { $in: ticketIds } });
+
+    return tickets.map(t => mapTicketData(t));
+  } catch (error) {
+    throw error
+  }
+};
+
+const resolver = {
+  createTicket: async args => {
+    try {
+      const ticket = new Ticket({
+        title: args.ticketInput.title,
+        description: args.ticketInput.description,
+        hiPri: args.ticketInput.hiPri,
+        creator: "5c9113a6afd82d35a45a2cf2",
+        label: args.ticketInput.label,
+        createdDate: new Date(args.ticketInput.createdDate)
+      });
+
+      new Fawn.Task()
+        .save('tickets', ticket)
+        .update('users', { _id: ticket.creator }, {
+          $push: { createdTickets: ticket._id }
+        })
+        .run();
+
+      return mapTicketData(ticket);
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  createUser: async args => {
+    try {
+      const oldUser = await User.findOne({ email: args.userInput.email });
+      if (!oldUser) throw new Error("User exists");
+
+      const hashedPwd = await bcrypt.hash(args.userInput.password, 12);
+      const userObj = new User({
+        email: args.userInput.email,
+        password: hashedPwd
+      });
+
+      const newUser = await userObj.save();
+
+      return newUser;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getTickets: async () => {
+    try {
+      const tickets = await Ticket.find();
+
+      return tickets.map(t => {
+        if (!t.assignee) return mapTicketData(t);
+
+        return mapTicketData(t, true)
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  getTicket: async ({ ticketId }) => {
+    try {
+      const ticket = await Ticket.findById(ticketId);
+      if (!ticket) throw new Error("Ticket not found!");
+
+      if (!ticket.assignee) return mapTicketData(ticket);
+
+      return mapTicketData(ticket, true);
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  assignTicket: async ({ userEmail, ticketId }) => {
+    try {
+      const user = await User.findOne({ email: userEmail });
+      const ticket = await Ticket.findById(ticketId);
+
+      new Fawn.Task()
+        .update('tickets', { _id: ticket._id }, {
+          $set: { assignee: user }
+        })
+        .update('users', { _id: user._id }, {
+          $push: { assignedTickets: ticket._id }
+        })
+
+      return {
+        ...ticket._doc,
+        assignee: queryUser.bind(this, user._id),
+        creator: queryUser.bind(this, ticket.creator)
+      }
+
+    } catch (err) {
+      throw err;
+    }
+  }
+};
+
+module.exports = resolver;
---
+++ graphql/schema/index.js
@@ -0,0 +1,55 @@
+const { buildSchema } = require('graphql')
+
+const schema = buildSchema(`
+  type Ticket {
+    _id: ID!
+    title: String!
+    description: String!
+    hiPri: Boolean!
+    label: String!
+    creator: User!
+    assignee: User
+    createdDate: String!
+  }
+
+  type User {
+    _id: ID!
+    email: String!
+    password: String
+    createdTickets: [Ticket!]!
+    assignedTickets: [Ticket!]!
+  }
+
+  input TicketInput {
+    title: String!
+    description: String!
+    hiPri: Boolean!
+    label: String!
+    creator: String!
+    createdDate: String!
+  }
+
+  input UserInput {
+    email: String!
+    password: String!
+  }
+
+  type RootQuery {
+    getTickets: [Ticket!]!
+    getTicket(ticketId: ID!): Ticket!
+    getUser: User!
+  }
+
+  type RootMutation {
+    createTicket(ticketInput: TicketInput): Ticket
+    createUser(userInput: UserInput): User
+    assignTicket(userEmail: String, ticketId: ID): Ticket
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`);
+
+module.exports = schema;
