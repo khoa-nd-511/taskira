5b89de8e930169a8c2e58fc72a2adcefb7169aab
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-03-25 00:06:22 +0700

    [backend] set header for res

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ if (!config.get('jwtPrivateKey')) {
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+})
+
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHTTP({
