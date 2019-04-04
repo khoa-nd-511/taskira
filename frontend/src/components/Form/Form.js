9cae3e9dee8409b742c923a02edd6967fc7a097f
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-04-05 00:03:28 +0700

    [frontend] generate dynamic input based on type

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,26 +1,117 @@
 import React from 'react';
 
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-
+import {
+  FormControl,
+  Input,
+  InputLabel,
+  FormControlLabel,
+  Switch,
+  RadioGroup,
+  Radio,
+} from '@material-ui/core';
 
 const formControl = props => {
 
-  const { formFields } = props;
+  const { formFields, switched, selected } = props;
 
   return Object.keys(props.formFields).map(f => {
-    const { type, placeholder, reference } = formFields[f];
+    const { name, type, placeholder, reference, options, checked } = formFields[f];
+
+    let input = null;
+
+    switch (type) {
+      case 'text':
+        input = (
+          <React.Fragment>
+            <InputLabel htmlFor={type}>{placeholder}</InputLabel>
+            <Input
+              id={type}
+              name={type}
+              type={type}
+              inputRef={reference}
+            />
+          </React.Fragment>
+        )
+        break;
+
+      case 'email':
+        input = (
+          <React.Fragment>
+            <InputLabel htmlFor={type}>{placeholder}</InputLabel>
+            <Input
+              id={type}
+              name={type}
+              type={type}
+              inputRef={reference}
+            />
+          </React.Fragment>
+        )
+        break;
+
+      case 'password':
+        input = (
+          <React.Fragment>
+            <InputLabel htmlFor={type}>{placeholder}</InputLabel>
+            <Input
+              id={type}
+              name={type}
+              type={type}
+              inputRef={reference}
+            />
+          </React.Fragment>
+        )
+        break;
+
+      case 'radio':
+        input = (
+          <FormControlLabel
+            control={
+              <Switch
+                checked={checked}
+                onChange={() => switched(!checked)}
+              />}
+            label="High Prioritized"
+            labelPlacement="top"
+          />
+        )
+        break;
+
+      case 'select':
+        const selectOptions = Object.keys(options).map(o => (
+          <FormControlLabel
+            key={options[o].label}
+            value={options[o].label}
+            label={options[o].label}
+            labelPlacement="top"
+            control={<Radio checked={options[o].selected} />}
+          />))
+        input = (
+          <RadioGroup
+            onChange={e => selected(e.target.value.toLowerCase())}
+          >
+            {selectOptions}
+          </RadioGroup>
+        )
+        break;
+
+      default:
+        input = (
+          <React.Fragment>
+            <InputLabel htmlFor={type}>{placeholder}</InputLabel>
+            <Input
+              id={type}
+              name={type}
+              type={type}
+              inputRef={reference}
+            />
+          </React.Fragment>
+        )
+        break;
+    }
 
     return (
-      <FormControl margin="normal" required fullWidth key={type}>
-        <InputLabel htmlFor={type}>{placeholder}</InputLabel>
-        <Input
-          id={type}
-          name={type}
-          type={type}
-          inputRef={reference}
-        />
+      <FormControl margin="normal" required fullWidth key={name}>
+        {input}
       </FormControl>)
   })
 
